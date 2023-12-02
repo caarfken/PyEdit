af917b59b83ac511541c4ac58a9021ec37b6d6fe
Corbitt Arfken <caarfken@protonmail.com>
2023-12-02 16:08:06 -0400

    bugfixes

--- install_debian.sh
+++ install_debian.sh
@@ -2,19 +2,20 @@
 
 echo Welcome to the PyEdit installer! Please run as root
 echo Installing Dependencies...
-apt-get install python3 pip git
+apt-get install python3 git
 echo Done
 echo Setting up file tree...
-cd ~
+cd /home/$SUDO_USER
 mkdir .PyEdit
 cd .PyEdit
 echo Done
 echo Downloading PyEdit...
+rm -rd PyEdit
 git clone https://github.com/caarfken/PyEdit.git
 echo Done
 echo Installing PyEdit
 cd PyEdit/PyEdit
-echo alias pyedit "python3 ~/.PyEdit/PyEdit/main.py" >> .bashrc
+echo alias pyedit=\"python3 /home/$SUDO_USER/.PyEdit/PyEdit/PyEdit/main.py\" >> /home/$SUDO_USER/.bashrc
 pwd
-mv PyEdit.desktop ~/.local/share/applications
+mv PyEdit.desktop /home/$SUDO_USER/.local/share/applications
 
